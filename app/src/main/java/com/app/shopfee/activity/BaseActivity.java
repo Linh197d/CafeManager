3e2114d34e08e94d093516659bfd0cefbf5d5b27
linhdq1 <linhdq@vtn-global.com>
2025-01-10 12:57:21 +0700

    update add security

--- app/src/main/java/com/app/shopfee/activity/BaseActivity.java
+++ app/src/main/java/com/app/shopfee/activity/BaseActivity.java
@@ -24,6 +24,8 @@ public abstract class BaseActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
+
+        ///
     }
 
     @Override
